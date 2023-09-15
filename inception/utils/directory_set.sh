c206b39cd7e1e5126f528d1864ddc4dfbbcc51da
hyeondle <st.linsio@gmail.com>
2023-09-16 06:49:59 +0900

    delete all

--- inception/utils/directory_set.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p srcs/requirements/mariadb/conf srcs/requirements/mariadb/tools
-mkdir -p srcs/requirements/nginx/conf srcs/requirements/nginx/tools
-mkdir -p srcs/requirements/wordpress
-touch srcs/docker-compose.yml srcs/.env
-touch srcs/requirements/mariadb/Dockerfile srcs/requirements/mariadb/.dockerignore
-touch srcs/requirements/nginx/Dockerfile srcs/requirements/nginx/.dockerignore
-touch srcs/requirements/wordpress/Dockerfile
